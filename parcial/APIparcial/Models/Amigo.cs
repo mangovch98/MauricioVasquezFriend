c76d804d1726f82a3196db12dffdc9bfae59377a
Mauricio Vásquez Chalup <mauriciovch98@gmail.com>
2019-04-06 06:45:18 -0700

    creacion de la clase Amigo y DataContext en API

---
+++ parcial/APIparcial/Models/Amigo.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIparcial.Models
+{
+    public enum AmiType
+    {
+        Conocido,
+        Compañero_Estudio,
+        Colega_de_Trabajo,
+        Amigo,
+        Amigo_de_Infancia,
+        Pariente
+    }
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+
+
+    public class Amigo
+    {
+        [Key]
+        public int AmigoID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+        public int Birthday { get; set; }
+        [Required]
+        public AmiType Ami {get; set;}
+        public StatusType Statuts { get; set; }
+
+    }
+}
